62311e80ad500ae6233dc8e9eb97b3df6ffaf563
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-24 20:02:24 +0530

    Added: welcome message

---
+++ SnakeAndLadder1/SnakeAndLadder1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome message");
+        }
+    }
+}
